fb394dd73bcc6efee902e0798e1e788c5908e598
WaifuForever <itachiut1r4@gmail.com>
2021-11-19 11:09:43 -0300

    add: 'A web API as type' complete

--- src/ApiType.hs
+++ src/ApiType.hs
@@ -23,12 +23,74 @@ type UserAPI =
 -- Either /users/list-all
 -- or     /list-all/users
 type UserAPI2 =
-     "users" 
-  :> "list-all" 
-  :> Get '[JSON] [User]
-  :<|> "list-all"
-  :> "users"
-  :> Get '[JSON] [User]
+  "users"
+    :> "list-all"
+    :> Get '[JSON] [User]
+    :<|> "list-all"
+    :> "users"
+    :> Get '[JSON] [User]
+
+-- /user/:userid
+-- except that we explicitly say that "userid" must be an integer
+type UserAPI5 =
+  "users"
+    :> Capture "userid" Integer
+    :> Get '[JSON] User
+    :<|> "user"
+    :> Capture "userid" Integer
+    :> DeleteNoContent -- equivalent to 'DELETE /user/:userid'
+
+-- GET /users/?sortby={age, name}
+type UserAPI6 =
+  "users"
+    :> QueryParam "sortby" SortBy
+    :> Get '[JSON] [User]
+
+-- POST /users
+-- PUT /users/:userid
+type UserAPI7 =
+  "users"
+    :> ReqBody '[JSON] User
+    :> Post '[JSON] User
+    :<|> "users"
+    :> Capture "userid" Integer
+    :> ReqBody '[JSON] User
+    :> Put '[JSON] User
+
+type UserAPI8 =
+  "users"
+    :> Header "User-Agent" Text
+    :> Get '[JSON] [User]
+
+type UserAPI10 =
+  "users"
+    :> Get
+         '[JSON]
+         (Headers '[Header "User-Count" Integer] [User])
+
+type ProtectedAPI11 =
+  UserAPI
+    :<|> BasicAuth "my-realm" User
+    :> UserAPI2
+
+-- embedded api
+type SubPathAPi =
+  "users"
+    :> (UserAPI2 :<|> UserAPI8)
+    :> Get '[JSON] User
+
+type UserAPI12 innerAPI =
+  UserAPI
+    :<|> "inner" :> innerAPI
+
+type UserAPI12Alone = UserAPI12 EmptyAPI
+
+type UserAPI13 =
+  "users"
+    :> Get '[JSON] [User]
+    :<|> Raw
+
+
 
 data SortBy = Age | Name
 
